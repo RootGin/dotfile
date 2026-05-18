cc4ec7a38cc5c57e470b34a4959b696230e00804
RootGin <rootgin@proton.me>
2026-05-18 16:23:55 +0700

    license and update gitignore

--- features/modules/desktop/hypr/hyprland.nix
+++ features/modules/desktop/hypr/hyprland.nix
@@ -92,9 +92,9 @@
             };
 
             general = {
-              gaps_in = "5";
-              gaps_out = "20";
-              border_size = "2";
+              gaps_in = "0";
+              gaps_out = "0";
+              border_size = "0";
               resize_on_border = false;
               allow_tearing = false;
               layout = "dwindle";
