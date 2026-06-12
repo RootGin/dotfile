30e6c62541916d514aa0e880eb2fe2f1d259eb86
RootGin <rootgin@proton.me>
2026-06-12 21:54:08 +0700

    zerotier

--- features/modules/security/vpn/default.nix
+++ features/modules/security/vpn/default.nix
@@ -1,10 +1,30 @@
 { self, inputs, ... }:
 {
   flake.nixosModules.modulesSecurityVpn =
-    { pkgs, ... }:
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
     {
       environment.systemPackages = [
         pkgs.proton-vpn
       ];
+
+      services.zerotierone = {
+        enable = true;
+      };
+
+      systemd.services.zerotierone = lib.mkIf config.security.agenix.enable {
+        postStart = ''
+          ${pkgs.zerotierone}/bin/zerotier-cli join "$(cat ${
+            config.age.secrets."zerotier-network-id".path
+          })" || true
+        '';
+      };
+
+      networking.firewall.allowedTCPPorts = [ 25565 ];
+      networking.firewall.allowedUDPPorts = [ 19132 ];
     };
 }
