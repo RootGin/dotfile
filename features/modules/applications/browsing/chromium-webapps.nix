1f5dfa6d3447e994d323a729fa4c67d7d6397f63
RootGin <rootgin@proton.me>
2026-06-23 21:55:08 +0700

    clean chromium

--- features/modules/applications/browsing/chromium-webapps.nix
+++ features/modules/applications/browsing/chromium-webapps.nix
@@ -1,17 +1,26 @@
 { self, ... }:
 {
   flake.nixosModules.applicationsBrowsingChromiumWebapps =
-    { config, pkgs, lib, ... }:
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
     let
-      mkChromiumApp = {
-        appName,
-        url,
-        desktopName ? appName,
-        profile ? appName,
-        class ? "crx_${appName}",
-        categories ? [ "Network" "WebBrowser" ],
-        icon ? "chromium"
-      }:
+      mkChromiumApp =
+        {
+          appName,
+          url,
+          desktopName ? appName,
+          profile ? appName,
+          class ? "crx_${appName}",
+          categories ? [
+            "Network"
+            "WebBrowser"
+          ],
+          icon ? "chromium",
+        }:
         let
           wrapper = pkgs.writeShellScriptBin "${appName}-web" ''
             exec ${pkgs.chromium}/bin/chromium \
@@ -20,6 +29,8 @@
               --class=${class} \
               --no-first-run --no-default-browser-check \
               --ozone-platform=wayland \
+              --enable-wayland-ime \
+              --wayland-text-input-version=3 \
               "$@"
           '';
           desktop = pkgs.makeDesktopItem {
@@ -38,42 +49,50 @@
     in
     {
       options.my.chromiumWebApps = lib.mkOption {
-        type = lib.types.attrsOf (lib.types.submodule ({ name, ... }: {
-          options = {
-            enable = lib.mkEnableOption "Enable this webapp";
-            url = lib.mkOption {
-              type = lib.types.str;
-            };
-            desktopName = lib.mkOption {
-              type = lib.types.str;
-              default = name;
-            };
-            profile = lib.mkOption {
-              type = lib.types.str;
-              default = name;
-            };
-            class = lib.mkOption {
-              type = lib.types.str;
-              default = "crx_${name}";
-            };
-            categories = lib.mkOption {
-              type = lib.types.listOf lib.types.str;
-              default = [ "Network" "WebBrowser" ];
-            };
-            icon = lib.mkOption {
-              type = lib.types.either lib.types.str lib.types.path;
-              default = "chromium";
-              description = "Icon name from system theme or path to an image file";
-            };
-          };
-        }));
+        type = lib.types.attrsOf (
+          lib.types.submodule (
+            { name, ... }:
+            {
+              options = {
+                enable = lib.mkEnableOption "Enable this webapp";
+                url = lib.mkOption {
+                  type = lib.types.str;
+                };
+                desktopName = lib.mkOption {
+                  type = lib.types.str;
+                  default = name;
+                };
+                profile = lib.mkOption {
+                  type = lib.types.str;
+                  default = name;
+                };
+                class = lib.mkOption {
+                  type = lib.types.str;
+                  default = "crx_${name}";
+                };
+                categories = lib.mkOption {
+                  type = lib.types.listOf lib.types.str;
+                  default = [
+                    "Network"
+                    "WebBrowser"
+                  ];
+                };
+                icon = lib.mkOption {
+                  type = lib.types.either lib.types.str lib.types.path;
+                  default = "chromium";
+                  description = "Icon name from system theme or path to an image file";
+                };
+              };
+            }
+          )
+        );
         default = { };
       };
 
       config = {
-        environment.systemPackages = lib.mapAttrsToList
-          (name: app: mkChromiumApp (lib.removeAttrs app [ "enable" ] // { appName = name; }))
-          (lib.filterAttrs (_: v: v.enable or true) cfg);
+        environment.systemPackages = lib.mapAttrsToList (
+          name: app: mkChromiumApp (lib.removeAttrs app [ "enable" ] // { appName = name; })
+        ) (lib.filterAttrs (_: v: v.enable or true) cfg);
 
         my.chromiumWebApps = {
           zalo = {
@@ -82,7 +101,11 @@
             desktopName = "Zalo";
             profile = "ZaloProfile";
             class = "chrome-chat.zalo.me__-ZaloProfile";
-            categories = [ "Network" "Chat" "InstantMessaging" ];
+            categories = [
+              "Network"
+              "Chat"
+              "InstantMessaging"
+            ];
             icon = ./icons/zalo.svg;
           };
 
@@ -92,7 +115,11 @@
             desktopName = "Telegram";
             profile = "TelegramProfile";
             class = "chrome-web.telegram.org__-TelegramProfile";
-            categories = [ "Network" "Chat" "InstantMessaging" ];
+            categories = [
+              "Network"
+              "Chat"
+              "InstantMessaging"
+            ];
           };
 
           protonmail = {
@@ -101,7 +128,10 @@
             desktopName = "Protonmail";
             profile = "ProtonmailProfile";
             class = "chrome-mail.proton.me__-ProtonmailProfile";
-            categories = [ "Network" "Email" ];
+            categories = [
+              "Network"
+              "Email"
+            ];
             icon = ./icons/proton-mail-seeklogo.svg;
           };
 
@@ -111,7 +141,11 @@
             desktopName = "Google Meet";
             profile = "GoogleMeetProfile";
             class = "chrome-meet.google.com__-GoogleMeetProfile";
-            categories = [ "Network" "Chat" "InstantMessaging" ];
+            categories = [
+              "Network"
+              "Chat"
+              "InstantMessaging"
+            ];
             icon = ./icons/Google_Meet_icon.svg;
           };
         };
